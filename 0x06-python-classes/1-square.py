08e984e0ebb76cd32db8c336135bcb44f8b09fe1
kadirine <mohamedmo3iz99@gmail.com>
2023-11-21 09:05:56 -0800

    welcome to 0x06. Python - Classes and Objects project

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """
+    This class defines a square with a private instance attribute 'size'.
+    """
+    def __init__(self, size):
+        """
+        Initializes a new Square instance with the given size.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size

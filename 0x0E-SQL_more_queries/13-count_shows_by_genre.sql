41f5c95f9a9244cd418a16ba6f6bc42741e458bb
kadirine <mohamedmo3iz99@gmail.com>
2024-01-18 18:04:29 -0800

    correct tasks 0x0E. SQL - More queries

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,12 @@
+-- This script lists all genres from 'hbtn_0d_tvshows' and displays the number of shows linked to each
+-- Each record displays: <TV Show genre> - <Number of shows linked to this genre>
+-- Genres without any shows linked are not displayed
+-- Results are sorted in descending order by the number of shows linked
+-- The script uses only one SELECT statement
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+GROUP BY tv_genres.name
+HAVING COUNT(tv_show_genres.show_id) > 0
+ORDER BY COUNT(tv_show_genres.show_id) DESC;
